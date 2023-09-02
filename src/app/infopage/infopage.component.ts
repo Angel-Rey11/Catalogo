4320a248169c5587ff498ce388d5cad142130881
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2023-09-03 00:58:57 +0200

    info

--- src/app/infopage/infopage.component.ts
+++ src/app/infopage/infopage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
+
+declare var bootstrap: any;
+
 
 @Component({
   selector: 'app-infopage',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfopageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private clipboardService: ClipboardService) { }
+
+  telefono1 : string = '639 728 158'
+  telefono2 : string = '651 483 122'
 
   ngOnInit(): void {
+  }
+
+  copyTelefono1ToClipboard() {
+    this.clipboardService.copy(this.telefono1);
+    const toastLiveExample = document.getElementById('copiado')
+    const toast = new bootstrap.Toast(toastLiveExample)
+    toast.show();
+  }
+
+  copyTelefono2ToClipboard() {
+    this.clipboardService.copy(this.telefono2);
+    const toastLiveExample = document.getElementById('copiado')
+    const toast = new bootstrap.Toast(toastLiveExample)
+    toast.show();
   }
 
 }
