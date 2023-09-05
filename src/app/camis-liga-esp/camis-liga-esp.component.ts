aa772ea9c3dd9381423f127d371e59b44c5f1d87
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2023-09-05 19:25:21 +0200

    todo

--- src/app/camis-liga-esp/camis-liga-esp.component.ts
+++ src/app/camis-liga-esp/camis-liga-esp.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage, listAll, getDownloadURL, ref } from '@angular/fire/storage';
 
+declare var bootstrap: any;
+
 @Component({
   selector: 'app-camis-liga-esp',
   templateUrl: './camis-liga-esp.component.html',
@@ -18,22 +20,46 @@ export class CamisLigaEspComponent implements OnInit {
 
   }
 
-  getImages(url:string) {
-    const imagesRef = ref(this.storage, 'camis/ligasEsp/' + url);
-
-    listAll(imagesRef)
-    .then(async response => {
-      this.images = [];
-      for(let item of response.items) {
-        const url = await getDownloadURL(item);
-        this.images.push(url);
-      }
-    })
-    .catch(error => console.log(error));
+  async getImages(url: string) {
+    return new Promise<void>((resolve, reject) => {
+      const imagesRef = ref(this.storage, 'camis/ligasEsp/' + url);
+  
+      listAll(imagesRef)
+        .then(async response => {
+          this.images = [];
+          for (let item of response.items) {
+            try {
+              const imageUrl = await getDownloadURL(item);
+              this.images.push(imageUrl);
+            } catch (error) {
+              reject(error);
+              return;
+            }
+          }
+          resolve();
+        })
+        .catch(error => {
+          console.log(error);
+          reject(error);
+        });
+    });
   }
 
-  openmodal(url:string) {
-    this.getImages(url);
+  async openmodal(url: string) {
+    try {
+      await this.getImages(url);
+      this.open();
+    } catch (error) {
+      console.error('Error al descargar las imágenes:', error);
+    }
+  }
+
+  open() {
+    var modal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+      keyboard: false
+    });
+    
+    modal.show();
   }
 
 }
