cae7a1f48f01fb80b3293c586ce41575f72ad4f0
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2023-09-08 20:23:32 +0200

    ajustesMenus

--- src/app/escuderias-menu/escuderias-menu.component.ts
+++ src/app/escuderias-menu/escuderias-menu.component.ts
@@ -7,11 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EscuderiasMenuComponent implements OnInit {
 
-  colorList: string[] = [
-    '#1F3F6E', '#00A19B', '#00594F', 'orange', 'red',
-    '#005AB9', '#A42134', '#20394C', '#00A0DE', 'gray'
-  ];
-
   constructor() { }
 
   ngOnInit(): void {
--- src/app/escuderiasmoto-menu/escuderiasmoto-menu.component.ts
+++ src/app/escuderiasmoto-menu/escuderiasmoto-menu.component.ts
@@ -7,11 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EscuderiasmotoMenuComponent implements OnInit {
 
-  colorList: string[] = [
-    'orange', 'rgb(34, 171, 235)', 'red', 'rgb(240, 87, 16)', 'rgb(204, 42, 13)',
-    'gray', 'rgb(170, 161, 161)', 'red', 'crimsom', 'rgb(167, 167, 15)', 'skyblue'
-  ];
-
   constructor() { }
 
   ngOnInit(): void {
